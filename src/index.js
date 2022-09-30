6f517bf94da4d135bc13f683bd0445735ef2f760
ramadhani25 <ramadhaniak1125@gmail.com>
2022-09-30 09:19:22 +0700

    cleaning code

--- src/index.js
+++ src/index.js
@@ -8,11 +8,9 @@ import reducers from "./reducers/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
   <Provider store={reducers}>
     <App />
   </Provider>
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
